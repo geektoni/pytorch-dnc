ab4d1b7085a8398f143c2dd8a09541d77f761b64
Giovanni De Toni <giovanni.det@gmail.com>
2019-09-25 11:26:08 +0200

    Use a Sequential model for the FFNN instead.

--- dnc/feedforward.py
+++ dnc/feedforward.py
@@ -1,25 +1,23 @@
 import torch
 import torch.nn as nn
+import collections
 
 # Fully connected neural network with multiple hidden layer
 class FFNN(nn.Module):
     def __init__(self, input_size, output_size, num_layers=1, bias=True):
         super(FFNN, self).__init__()
-        self.fc1 = nn.Linear(input_size, output_size+1, bias=bias)
-        self.sigm = nn.Sigmoid()
-        self.layers = []
+        self.layers = collections.OrderedDict()
+        self.layers["layer_input"] = nn.Linear(input_size, output_size+1, bias=bias)
+        self.layers["sigmoid"] = nn.Sigmoid()
         for i in range(0, num_layers):
             if num_layers==1:
                 break
-            self.layers.append(nn.Linear(output_size+1, output_size+1, bias=bias))
-        self.fc2 = nn.Linear(output_size+1, output_size, bias=bias)
+            self.layers["hidden_{}".format(i+1)] = nn.Linear(output_size+1, output_size+1, bias=bias)
+            self.layers["sigmoid_{}".format(i+1)] = nn.Sigmoid()
+        self.layers["layer_output"] = nn.Linear(output_size+1, output_size, bias=bias)
+        self.model = nn.Sequential(self.layers)
 
     # The parameter chx is maintained, but it is ignored
     def forward(self, x, chx):
-        out = self.fc1(x)
-        out = self.sigm(out)
-        for i in range(0, len(self.layers)):
-            out = self.layers[i](out)
-            out = self.sigm(out)
-        out = self.fc2(out)
+        out = self.model.forward(x)
         return out, chx
